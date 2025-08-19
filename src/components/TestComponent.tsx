130bdec2546a0128a75a0881494c111c5fcac728
MengAl <alex.meng@ontario.ca>
2025-08-19 16:47:41 -0400

    Fix tailwind config

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestComponent = () => {
+  return (
+    <div className="p-4 bg-red-500 text-white rounded-lg">
+      <p className="text-lg font-bold">Test Component</p>
+      <p className="mt-2">If you can see this with red background and white text, Tailwind CSS is working!</p>
+    </div>
+  );
+};
+
+export default TestComponent;
